ec7b311f7f951f4d0b5bdd9e57dd1e42535f7556
Jonathan Casey <Jonathan.J.Casey@gmail.com>
2016-03-14 20:34:57 -0400

    Renamed app from 'ladder to 'league.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_ping_pong_ladder_session'
+Rails.application.config.session_store :cookie_store, key: '_ping_pong_league_session'
